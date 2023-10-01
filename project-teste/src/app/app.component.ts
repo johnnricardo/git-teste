17deb5996e8dd69570a7cacb3a045196fd8306f1
johnnricardo <joaoricardomoura99@gmail.com>
2023-10-01 13:39:27 -0300

    initial commit

---
+++ project-teste/src/app/app.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `<h1>Git teste</h1>`,
+})
+export class AppComponent {
+  title = 'project-teste';
+}
